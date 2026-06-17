751bee5cc6e9a22c8dba5a00fa9346a59426d019
arrya-sridhar <arryasridhar.v@gmail.com>
2026-06-17 13:32:29 +0530

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int p1=0,p2=0,curr;
+        for(int i=0;i<n;i++){
+            curr = max(p1,nums[i]+p2);
+            p2 = p1;
+            p1 = curr;
+        } 
+        return curr;
+    }
+};
